e81ec9e0e862c78c9791628d4ffe8fd9bd57e98c
蔚然 吴 <2241105683@qq.com>
2023-07-29 15:51:50 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace JZ计算机软件开发语言
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String lineCode,Code=null;
+            List<string> lines = new List<string>
+            (File.ReadAllLines(@"D:\jz.jz"));
+            int line = lines.Count;
+            for(int i = 0; i < line; i++)
+            {
+                lineCode = lines[i];
+                lines[i] = database.translateJzCode(lineCode);
+            }
+            for(int i = 0;i < line; i++)
+            {
+                Code = Code + lines[i]+"\n";
+            }
+            File.WriteAllText(@"D:\jz.java",Code);
+            cmdProcessStart(@"D:\jz.jz");
+            
+
+
+        }
+        public void cmdProcessStart(String Path)
+        {
+            Path = Path.Replace(".jz", ".java");
+            if (Path.Contains("jz") == true)
+            {
+                
+                if (File.Exists(Path) == true)
+                {
+                    Process cmdProcess = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Arguments = "/k java " + Path + " & echo jz计算机" +
+                        "软件编程语言由得快科技开发制作 & echo 若发现bug请联系QQ:2241105683";
+
+                    startInfo.RedirectStandardOutput = false;
+                    startInfo.UseShellExecute = true;
+                    startInfo.CreateNoWindow = false;
+
+                    cmdProcess.StartInfo = startInfo;
+                    cmdProcess.Start();
+                }
+                else
+                {
+                    MessageBox.Show("您选择的代码文件不存在");
+                }
+            }
+            else
+            {
+                Path = @"C:\temp\jz\jz.jz";
+                MessageBox.Show("未能正确识别的jz文件\n请检查文件后缀名是否正确");
+            }
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JZ计算机软件开发语言
+{
+    internal class database
+    {
+        static String[] englishData = {"public class", "public " +
+                "static void main(String[] args)","public", 
+            "private","byte","char","int","long","float",
+            "double","String","boolean","if","else if",
+            "else","while","for","System.out.println",
+            "System.out.print"
+        };
+        //todo "for(int i = 0;i <= x;i++)"--计次循环（满足传入x参数与可以返回当前计次个数的变量)
+        //do while--循环后判断
+        //需要后期重构
+
+        static String[] chineseData = {"#主类", "#程序入口","#公开",
+            "#私有","#字节型","#字符型","#整数型","#长整数型"
+                ,"#单精度小数型","#双精度小数型","#文本型",
+            "#布尔型","#如果","#否则如果","#否则",
+            "#判断后循环","#变量循环","#打印并换行","#打印"
+
+        };
+        public static string translateJzCode(string lineJzCode)
+        {
+            for (int i = 0; i < englishData.Length; i++)
+            {
+                string temp = chineseData[i];
+                bool state = lineJzCode.Contains(temp);
+                if (state == true)
+                {
+                    temp = englishData[i];
+                    lineJzCode = lineJzCode.Replace(chineseData[i], temp);
+                }
+            }
+            return lineJzCode;
+        }
+    }
+}
